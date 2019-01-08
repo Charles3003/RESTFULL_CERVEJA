612990dabd42293a533e1401eb404bdcbdfe6325
Charles Marques <charlesdevelop@hotmail.com>
2019-01-08 16:59:00 -0200

    submit

---
+++ MD_RESTFULL_CERVEJA/MD_RESTFULL_CERVEJA/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace MD_RESTFULL_CERVEJA.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ MD_RESTFULL_CERVEJA/MD_RESTFULL_CERVEJA/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MD_RESTFULL_CERVEJA.Startup))]
+
+namespace MD_RESTFULL_CERVEJA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
